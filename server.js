4d4f2e08948792f6fb37c4c8e262f4293cac8637
alfredo pedroza <alfredopedrozahernandez13@gmail.com>
2020-10-18 16:08:28 -0700

    server

---
+++ server.js
@@ -0,0 +1,31 @@
+//import dependencies
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+
+const app = express();
+
+const PORT = process.env.PORT || 8080;
+
+//connect to server
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
+
+//middleware
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(logger("dev"));
+// app.use(express.Router);
+
+//import routes
+app.use(require("./routes"));
+
+//start the server
+app.listen(PORT, () => {
+  console.log(`App running on PORT ${PORT}`);
+});
